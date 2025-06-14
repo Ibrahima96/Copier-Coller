a083ff3f25bc8fa828758acffdf55fd0eda00684
Ibrahima96 <ibrahimadiagne0811@gmail.com>
2025-06-14 19:00:50 +0000

    clipboard-navigator

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-const btn = document.querySelector("button");
-btn.addEventListener("click", handleClick);
+const copyClipboardButton = document.querySelector(".copy-clipboard-btn");
+copyClipboardButton.addEventListener("click", handleClick);
+let runningAnimation = false;
+function handleClick(e) {
+  navigator.clipboard.writeText(e.target.previousElementSibling.textContent);
+  if (!runningAnimation) {
+    
+    runningAnimation=true
 
+     e.target.textContent = "copiée 🎊";
 
+     setTimeout(() => {
+        e.target.textContent = "Copier 📚";
+        runningAnimation = false;
+     }, 1000);
+
+  }
+}
